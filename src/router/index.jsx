78b804b6e66107ee198c9f6a0a071f9f2c003a69
Wuhaofan <1071369315@qq.com>
2020-05-17 19:10:07 +0800

    xxxx

--- src/router/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { renderRoutes } from 'react-router-config'
-import { BrowserRouter } from 'react-router-dom'
-import routes from './route'
-const Router = () => {
-    return <BrowserRouter>{renderRoutes(routes)}</BrowserRouter>
-}
-export default Router;
